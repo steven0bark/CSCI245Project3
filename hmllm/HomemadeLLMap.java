8a7e1a03d5727958e963c2171054cd8082d11083
Steven <steven.barker@my.wheaton.edu>
2020-02-15 16:43:45 -0600

    Wrote all but remove method

--- hmllm/HomemadeLLMap.java
+++ hmllm/HomemadeLLMap.java
@@ -15,14 +15,18 @@ import java.util.Iterator;
 
 public class HomemadeLLMap implements HomemadeMap {
 
+	/**
+	 * The head of the map
+	 */
+    private Node head, tail;
 	
-    /**
+	/**
      * Test whether an association exists for this key.
      * @param key The key to remove
      * @return true if there is an association for this key, false otherwise
      */
     public boolean containsKey(String key) {
-    	return true;
+    	return head.findNode(key) != null;
     }
    
 
@@ -32,6 +36,18 @@ public class HomemadeLLMap implements HomemadeMap {
      * @param val The value to which this key is associated
      */
     public void put(String key, String val) {
+    	if(head != null) {
+    		Node n = head.findNode(key);
+    		if(n == null) {
+    			tail.setNext(new Node(key, val, null));
+    			tail = tail.next();
+    		}else {
+    			n.setvalue(val);
+    		}
+    	}else {
+    		head = new Node(key, val, null);
+    		tail = head;
+    	}
     }  
 
     /**
@@ -40,15 +56,51 @@ public class HomemadeLLMap implements HomemadeMap {
      * @return The value associated with this key, null if none exists
      */
     public String get(String key) {
-    	return null;
+    	if(head != null) {
+    		Node n = head.findNode(key);
+    		if(n != null)
+    			return n.value();
+    	}
+    	return "";
     }
 
     /**
-     * Get an iterator for all the keys in this map.
+     * Creates an Iterator object to iterate through the list.
+     * Creates an anonymous class inside of the method call that defines all necessary
+     * methods for an iterator, and the returns an instance of that iterator
+     * 
      * @return An iterator over the set of keys.
      */
     public Iterator<String> keyIterator() {
-    	return null;
+    	return new Iterator<String>() {
+    		/**
+    		 * Holds where we are in the list
+    		 */
+    		private Node place = head;
+    		
+    		/**
+    		 * Returns true if there is a next node, false if not
+    		 */
+			public boolean hasNext() { return place.next() != null; }
+
+			/**
+			 *  Determines if there is a next in the list. 
+			 *  If there is then it returns the key and sets the place to next for next call.
+			 *  Otherwise it returns an empty string.
+			 */
+			@Override
+			public String next() {
+				String toreturn = "";
+				if(hasNext()) {
+					toreturn = place.key();
+					place = place.next();
+					return toreturn;
+				}
+					return toreturn;
+				
+			}
+    		
+    	};
     }
 
     
@@ -57,8 +109,18 @@ public class HomemadeLLMap implements HomemadeMap {
      * @param key The key to remove
      */   
     public void remove(String key) {
+    	
     }
 
+    /**
+     *Returns the Node that contains a key, or returns null when that key does not exists 
+     */
+    public Node findNode(String k) {
+    	if(head != null) 
+    		return head.findNode(k);
+    	return null;
+    }
+    		
 
 	
 }
---
+++ hmllm/Node.java
@@ -0,0 +1,92 @@
+package hmllm;
+
+/**
+ * Node
+ * 
+ * This is the class for making items in the map, containing both
+ * the key and the value.
+ * 
+ * @author stevenbarker
+ */
+public class Node {
+
+	/**
+	 * The key in the map
+	 */
+	private String key;
+	
+	/**
+	 * the value in the map
+	 */
+	private String value;
+	
+	/**
+	 * The reference to the next node in the list
+	 */
+	private Node next;
+	
+	/**
+	 * Constructor
+	 * 
+	 * Initializes all necessary instance variables to create a node in the list
+	 * @param k The key in the map.
+	 * @param v The value in the map.
+	 * @param n The reference to the next node in the list.
+	 */
+	public Node(String k, String v, Node n) {
+		key = k;
+		value = v;
+		next = n;
+	}
+
+	/**
+	 * getNext gives the next node in the list.
+	 * @return the next Node in the list.
+	 */
+	public Node next() {
+		return next;
+	}
+	/**
+	 * setNext sets a node object to be the next node after the calling object
+	 * @param n The new node to be set to the next node after the calling object.
+	 */
+	public void setNext(Node n) {
+		next = n;
+	}
+
+	/**
+	 * getKey gets the key
+	 * @return the key
+	 */
+	public String key() {
+		return key;
+	}
+
+	/**
+	 * getValue gets the value
+	 * @return the value associated with the key
+	 */
+	public String value() {
+		return value;
+	}
+
+	/**
+	 * setValue sets a value to be associated with a key
+	 * @param v The value to be associated to the string
+	 */
+	public void setvalue(String v) {
+		value = v;
+	}
+    
+    /**
+     * 
+     */
+    public Node findNode(String k) {
+    	while(next != null) {
+    		if(key.equals(k)) 
+    			return this;
+    	}
+    	return null;
+    }
+}
+
